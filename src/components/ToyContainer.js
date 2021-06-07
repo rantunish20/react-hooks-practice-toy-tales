956ffab404621b4c88bcbc6b622518abb5522c22
Romel <rantunish20@gmail.com>
2021-06-06 23:41:59 -0400

    Done

--- src/components/ToyContainer.js
+++ src/components/ToyContainer.js
@@ -1,9 +1,18 @@
 import React from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer() {
+function ToyContainer({toys, deleteToy, updateToy}) {
+  const toycards = toys.map((toy) => (
+    <ToyCard 
+    key={toy.id}
+    toy={toy}
+    deleteToy={deleteToy}
+    updateToy={updateToy}
+    />
+    ));
+
   return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
+    <div id="toy-collection">{toycards}</div>
   );
 }
 
